16345285826847508efcce21cf2c243ea620835e
trungpd56 <trungpd56@gmail.com>
2021-12-26 15:31:40 +0700

    update stdout

--- pingscript.py
+++ pingscript.py
@@ -13,14 +13,14 @@ while True:
         total = 0
         with open('iplist', 'r') as f:
                 for line in f:
-                        with open(os.devnull, "wb") as limbo:
-                                total += 1
-                                iphost = line.strip()
-                                result = subprocess.Popen(["ping", "-n", "1", "-w", "1000", iphost],
-                                                          stdout=limbo, stderr=limbo).wait()
-                                if result:
-                                        print(f'{iphost} inactive')
-                                        inactive += 1
+                        total += 1
+                        iphost = line.strip()
+                        result = subprocess.run(["ping", "-n", "1", "-w", "1000", iphost], capture_output=True)
+                        print(result.stdout.decode())
+                        print(result.stderr.decode())
+                        if result.returncode:
+                                print(f'{iphost} inactive')
+                                inactive += 1
         print(f"{t}---{inactive}/{total} host is inactive")
         print("*" * 23)
         print("\n")
